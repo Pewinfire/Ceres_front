3686477657ab856c05c2346b6f9bd3139545c31a
Pewinfire <s.dung@hotmail.es>
2022-01-20 13:21:34 +0100

    geo

--- src/market/components/MarketItem.js
+++ src/market/components/MarketItem.js
@@ -1,25 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import Avatar from "../../shared/components/UIElements/Avatar";
+import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
+import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
-import "./MarketItem.css"
+
+import "./MarketItem.css";
 
 const MarketsItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className="market-item">
+    /*  <li className="market-item">
       <Card className="market-item__content">
         <Link to={`/${props.id}/shops`}>
           <div className="market-item__image">
-            <Avatar image={`${process.env.REACT_APP_BACKEND_IMG}/${props.image}`} alt={props.name} />
+            <img
+              src={`${process.env.REACT_APP_BACKEND_IMG}/${props.image}`}
+              alt={props.name}
+            />
           </div>
           <div className="market-item__info">
             <h2>{`${props.name}`}</h2>
             <h3>{`${props.address}`}</h3>
-         
           </div>
         </Link>
       </Card>
-    </li>
+    </li> */
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="market-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+      >
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
+        </div>
+      </Modal>
+      <li className="market-item">
+        <Card className="market-item__content">
+          <div className="market-item__image">
+            <img
+              src={`${process.env.REACT_APP_BACKEND_IMG}/${props.image}`}
+              alt={props.name}
+            />
+          </div>
+          <div className="market-item__info">
+            <h2>{props.name}</h2>
+            <h3>{props.address}</h3>
+          </div>
+          <div className="market-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              View on map
+            </Button>
+            <Button to={`/markets/${props.id}`}>Ir</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 };
 
