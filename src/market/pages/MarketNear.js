269859d0b8401b884d37f8eb4dcf0675cc7887df
Pewinfire <s.dung@hotmail.es>
2022-02-13 06:15:01 +0100

    market/shop

--- src/market/pages/MarketNear.js
+++ src/market/pages/MarketNear.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import MarketList from "../components/MarketList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useParams } from "react-router-dom";
 import { useHttpClient } from "../../shared/hooks/http-hook";
+import { Pagination } from "@mui/material";
 import { Box } from "@mui/system";
 import Input from "@mui/material/Input";
 
 const MarketNear = () => {
-  const [loadedMarkets, setLoadedMarkets] = useState();
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedMarkets, setLoadedMarkets] = useState();
   const [search, setSearch] = useState("merca");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [size, setSize] = useState(3);
+
   const address = useParams().addr;
 
   useEffect(() => {
@@ -18,32 +23,38 @@ const MarketNear = () => {
     const fetchMarkets = async () => {
       try {
         const responseData = await sendRequest(
-          `${process.env.REACT_APP_BACKEND_URL}/market/near/${address}`
+          `${process.env.REACT_APP_BACKEND_URL}/market/near/${address}/${
+            page - 1
+          }/${size}/${search}`
         );
 
         setLoadedMarkets(responseData.markets);
+        setTotalPages(responseData.totalPages);
       } catch (err) {}
     };
     fetchMarkets();
-  }, [sendRequest, address]);
+  }, [sendRequest, search, address, page, size, totalPages]);
 
-
-  const handleTextFieldKeyDown = event => {
+  const handleTextFieldKeyDown = (event) => {
     switch (event.key) {
-        case 'Enter':
-          if(event.target.value){
-          setSearch(event.target.value);}
-          else{
-            setSearch("merca")
-          }
-            break
-        case 'Escape':
-          setSearch("merca")
-            break
-        default: break
+      case "Enter":
+        if (event.target.value) {
+          setSearch(event.target.value);
+        } else {
+          setSearch("merca");
+        }
+        break;
+      case "Escape":
+        setSearch("merca");
+        break;
+      default:
+        break;
     }
-}
+  };
 
+  const selectPage = (event, value) => {
+    setPage(value);
+  };
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
@@ -69,7 +80,15 @@ const MarketNear = () => {
               onKeyDown={handleTextFieldKeyDown}
             />
           </Box>
-          <MarketList items={loadedMarkets} />
+          <MarketList items={loadedMarkets} />{" "}
+          <Pagination
+            count={totalPages}
+            page={page}
+            className="pagination"
+            color="success"
+            onChange={selectPage}
+            size="large"
+          />
         </div>
       )}
     </React.Fragment>
